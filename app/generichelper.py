c15632634092bae5159e725a26d004596ea15026
Inish Codes <inishbista20@gmail.com>
2025-10-25 23:03:02 -0400

    Chore: Create app directory as main directory with main FastAPI server and helper scripts

---
+++ app/generichelper.py
@@ -0,0 +1,102 @@
+from typing import Any, Dict, List #for type hints
+def get_str_from_food_dict(food_dict:Dict[str,Any]):
+    return ",".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+
+# if __name__ == "__main__":
+#     print(get_str_from_food_dict({"momo": 2, "thali": 1}))
+
+# Nomralize both variables..#dialogflow returns single variable as int or float, not list..add momo --> quantity =
+    # 1.0 , not [1]
+def NormalizeVariable(item, quantities):
+    if isinstance(item, str):
+        item = [] if item.strip()== "" else [item]
+    if isinstance(quantities, (int, float)):
+        quantities = [quantities]
+    elif isinstance(quantities, str):
+        quantities = [] if quantities.strip() == "" else [quantities]
+    return item,quantities  #return tuple
+
+def ReturnFulfillmentMessage(order_dict):
+    order_str = get_str_from_food_dict(order_dict)
+    fulfillmentText = f"So far you have: {order_str}. Do you need anything else?"
+    print("🧾 Current in_progress_order:", order_dict)  # for debugging
+    return {
+        "fulfillmentText": fulfillmentText
+    }
+
+
+#Remove items from a particular session_id
+def removeItems(item, quantities, session_id, in_progress_order):
+    removable_item_dict = dict(zip(item, quantities))
+    if session_id not in in_progress_order:
+        return {
+            "fulfillmentText": "I having a trouble finding your order. Please place your order again"
+        }
+    # Else start deleting
+    order_dict = in_progress_order[session_id]
+    removedItems = []
+    absentItems = []
+    for item, qty_to_remove in removable_item_dict.items():
+        if item in order_dict:
+            if order_dict[item] > qty_to_remove:
+                order_dict[item] -= qty_to_remove
+            else:  # remove completely the item and its value
+                del order_dict[item]
+            removedItems.append(item)
+        else:
+            absentItems.append(item)
+
+    # BUILD THE MESSAGE telling about Deleted and Absent Items together in a Message
+    message_parts = []
+    if removedItems:
+        message_parts.append(f"Removed: {', '.join(removedItems)}")
+    if absentItems:
+        message_parts.append(f"Not in your order: {' ,'.join(absentItems)}")
+    if len(order_dict)!=0:
+        message_parts.append(f" Your cart now looks like this:  {showItems(order_dict)}")
+    else:
+        message_parts.append(f"Your cart is now empty")
+    return {
+        "fulfillmentText": " . ".join(message_parts)
+    }
+
+def showItems(order_dict):
+    joined_str = ", ".join(f"{int(q)} {i}"  for i, q in order_dict.items())
+    return joined_str
+
+
+
+def validate_session_id_and_retrieve_order(session_id, in_progress_order):
+    if session_id not in in_progress_order:
+        return {
+            "fulfillmentText": "I'm having a trouble finding your order. Sorry! Can you place a new order please?"
+        }
+    order_dict = in_progress_order[session_id]
+    return order_dict
+
+
+def   check_item_in_the_order_list(order_dict, item):
+    #item is a list , not a dictionary so retrieve value from the list
+    if isinstance(item, list) and len(item)>0:#there must be a item
+        item = item[0] #only for 1 item for now, convert list to the single string
+    return item in order_dict #if present , returns true , if not false
+
+
+def convert_list_to_string(List:list):
+    return " , ".join(items for items in List)
+
+
+#Instead of asking track_id again, derive it from previous track.order context in output context
+def derive_track_id_from_output_context(req_json:dict):
+    output_contexts = req_json["queryResult"]["outputContexts"]
+
+    order_id = None
+    for ctx in output_contexts: #there will be list of multiple output contexts
+        if "track_context" in ctx["name"]:
+            order_id = ctx["parameters"].get("order_id")
+            break
+
+    return order_id  # 👉 2
+
+
+
---
+++ app/main.py
@@ -0,0 +1,264 @@
+# IMPORT PACKAGES
+from fastapi import FastAPI , Request #Request to validate type hint of JSON request
+from pydantic import BaseModel
+from typing import Any, Dict, List #for type hints
+from collections import Counter
+import generichelper
+import dbOperations
+
+
+app = FastAPI()
+
+
+in_progress_order = {}
+
+
+def handle_order_add(parameters:Dict[str, Any], session_id:str, query_text:str) -> Dict[str, Any]:
+    item =  parameters.get("food_items", [])
+    quantities = parameters.get("quantity",[])
+
+    #Normalize both variables to remove "" for invalid item and quantity
+    item, quantities = generichelper.NormalizeVariable(item, quantities)
+
+    if len(item)==0:
+        return{
+            "fulfillmentText": "Which item you want to add?"
+        }
+
+
+    if len(item)!= len(quantities):
+        return{
+        "fulfillmentText" : "Sorry I didn't understand. Can you please specify the food items and quantity properly "}
+
+    #later try to add multiple items together , 1 lassi and 2 momo
+    else:
+       new_food_dictionary = dict(zip(item, quantities))
+    if session_id in in_progress_order: #USE EXISTING SESSION
+        #Check if user is trying to make an update or add more
+        update_keywords = ["change", "instead", "make", "replace", "no", "not", "update", "only"]
+        if any(word in query_text for word in update_keywords):
+            #REPLACEMENT
+            in_progress_order[session_id].update(new_food_dictionary)
+        else:
+            #INCREMENT
+            in_progress_order[session_id] = dict(Counter(in_progress_order.get(session_id, {})) + Counter(new_food_dictionary))  # use Counter when we want to comnbine multiple key's values together
+    else: #NEW SESSION_ID WILL BE CREATED BY PYTHON DICT
+        in_progress_order[session_id]= new_food_dictionary
+    return generichelper.ReturnFulfillmentMessage(in_progress_order[session_id])
+
+
+
+
+def handle_order_remove(parameters: Dict[str, Any], session_id:str, query_text:str, all_params_present:bool)-> Dict[
+    str,
+Any]:
+    item = parameters.get("items", [])
+    quantities = parameters.get("quantity", [])
+
+    # Normalize both variables
+    item, quantities = generichelper.NormalizeVariable(item, quantities)
+    keywords_for_all = ["all", "everything", "entire", "whole"]
+
+
+    #CONDITION #1 CHECK VALIDITY OF ITEMS
+    if len(item) ==0:
+        return{
+            "fulfillmentText" : "Which item do you want to remove? "
+        }
+    order_dict = generichelper.validate_session_id_and_retrieve_order(session_id, in_progress_order)
+    result = generichelper.check_item_in_the_order_list(order_dict, item)
+    if not result:
+        return {
+            "fulfillmentText": f"{generichelper.convert_list_to_string(item)} isn’t in your cart. Please check the "
+                               f"name or "
+                               f"add it "
+                               f"first."
+        }
+
+    #CONDITION#2 CHECK VALIDITY OF QUANTITIES
+    if not quantities:
+    # either we got "random char" or "all keyword" , we cant discard all keyword
+        if any(word in query_text for word in keywords_for_all):
+            #get existing quantity for  item from the session_dict
+            existing_items = in_progress_order.get(session_id, {})
+            quantities = [existing_items.get(i, 0) for i in item]
+            return generichelper.removeItems(item, quantities, session_id, in_progress_order)
+        #if any random character entered like ("a" , "ermm" instead of valid integer for quantity)
+        return {
+            "fulfillmentText": f"How many { ','.join(item)} you want to remove? "
+        }
+
+
+#Formality CHeck if multiple items entered together, every item should have corresponding quantities
+    if len(item)!= len(quantities):
+        return {
+            "fulfillmentText": "Sorry I didn't understand. Can you please specify the food items and quantity "
+                               "properly please'"
+        }
+    #CONDITION 3
+    return generichelper.removeItems(item, quantities, session_id, in_progress_order)
+
+
+
+def handle_order_reset(session_id:str):
+    #Clear all items from given current session
+    in_progress_order[session_id].clear()
+    return{
+        "fulfillmentText": "Your cart is cleared."
+    }
+
+
+def save_to_db(order: dict):
+    #get next order_id from database
+    next_order_id = dbOperations.get_next_order_id()
+    if next_order_id == -1:
+        return -1
+    for food_item, quantity in order.items():
+       result =  dbOperations.insert_order_item( next_order_id, food_item, quantity)
+       print(f"{food_item} inserted")
+       if result == -1:
+           return -1
+
+    return next_order_id
+
+
+
+def handle_order_display(session_id):
+    order_dict = generichelper.validate_session_id_and_retrieve_order(session_id, in_progress_order)
+    if len(order_dict)!=0:
+        return {
+            "fulfillmentText": f"Your cart currently looks like this:  {generichelper.showItems(order_dict)} "
+        }
+    return{
+        "fulfillmentText": "Your cart is empty at the moment. 🪄 Add something tasty to get started!"
+    }
+        
+        
+
+
+def handle_order_complete(parameters: Dict[str, Any], session_id:str)-> Dict[str, Any]:
+    order_dict = generichelper.validate_session_id_and_retrieve_order(session_id, in_progress_order)
+    order_id = save_to_db(order_dict)
+    if order_id == -1:
+        return{
+            "fulfillmentText": "Sorry, I couldn't process your order due to a backend error. " \
+            "Please place a new order again"
+        }
+    #Insertion is successful so does trigger for total_price
+    order_total_price = dbOperations.get_total_order_price(order_id)
+    del in_progress_order[session_id] #Remove the order from the buffer storage since it has been already stroed in
+    # the database
+    return{
+        "fulfillmentText":  f"Awesome. We have placed your order. "\
+                            f"Here is your order id #{order_id}. " \
+                            f"'Your order total is ${order_total_price} which you can pay at the time of delivery!"
+        }
+
+
+
+
+
+def handle_order_track( parameters: Dict[str, Any]):
+    track_id = parameters.get("order_id")
+    #Validate whether the track_id is int or not
+    try:
+        track_id = int(track_id)  # raises ValueError...if it isnt empty string then it will get typecast else
+        # ValueError
+    except ValueError:
+        return{
+            "fulfillmentText": "Enter a valid track id number"
+        }  # ✅ safely caught
+    #CHeck whether the track_id exist in the database or not
+    status = dbOperations.validateTrackID(track_id)
+    print(f"{status}status")
+    if status == -1:
+        return{
+                "fulfillmentText": "Hmm, it looks like there's no order linked to that tracking ID. Please enter the correct one."
+        }
+    return{
+        "fulfillmentText": f"Your order is {status}"
+    }
+
+
+
+def handle_tracked_order_display(req_json):
+    track_id = generichelper.derive_track_id_from_output_context(req_json)
+    #if unable to retrieve the order_id from  output context, prompt again for order_id
+    if not track_id:
+        return{
+        "fulfillmentText": "Yo, Enter the track_id again "
+        }
+    total_order_price = dbOperations.get_total_order_price(track_id)
+    #get item names and corresponding quantities
+    rows = dbOperations.extract_saved_order_from_db(track_id)
+    joined_str = ", ".join(f" {qty} {name}" for  name, qty in rows)
+    final_message = (f"Your order with track_id #{track_id} consists of {joined_str} and total price is "
+                     f"${total_order_price} ")
+    print(final_message)
+    return{
+        "fulfillmentText":final_message
+    }
+
+
+
+
+
+@app.get("/")
+def root():
+    return {"message": "FoodChatbot  is running 🚀"}
+#Main WEBHOOK HANDLER
+
+@app.post("/")
+async def handle_request(request: Request):
+    req_json = await request.json()
+
+#EXTRACTION
+
+    intent = req_json.get("queryResult", {}).get("intent", {}).get("displayName")
+    parameters = req_json.get("queryResult", {}).get("parameters", {})
+    query_text = req_json.get("queryResult", {}).get("queryText", "")
+    all_params_present = req_json.get("queryResult", {}).get("allRequiredParamsPresent", False)
+
+    session_path = req_json.get("session", "")
+    session_id = session_path.split("/sessions/")[-1]
+
+    intent_handle_dict = {
+    "Order.Add-context:ongoing-order" : handle_order_add,
+    "Order.remove-context:ongoing-order": handle_order_remove,
+    "Order.reset-context:ongoing-order": handle_order_reset,
+    "order.track-context:ongoing-order" : handle_order_track,
+    "Order.complete-context:ongoing_order": handle_order_complete,
+    "Order.display-context:ongoing-order": handle_order_display,
+    "Order.display_tracked_items": handle_tracked_order_display
+    }
+    if intent == "Order.Add-context:ongoing-order":
+        return intent_handle_dict[intent](parameters, session_id, query_text)
+    elif intent == "Order.remove-context:ongoing-order":
+        return intent_handle_dict[intent](parameters, session_id, query_text, all_params_present)
+    elif intent == "Order.reset-context:ongoing-order":
+        return intent_handle_dict[intent](session_id)
+    elif intent == "order.track-context:ongoing-order":
+        return intent_handle_dict[intent](parameters)
+    elif intent ==  "Order.display-context:ongoing-order":
+        return intent_handle_dict[intent](session_id)
+    elif intent == "Order.display_tracked_items":
+        return intent_handle_dict[intent](req_json)
+    else:
+        return intent_handle_dict[intent](parameters, session_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
